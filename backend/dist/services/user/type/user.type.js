744d77cd6bca6f744bef7e5ec594b31104aba313
ramiroguillen <ramirojguillen@gmail.com>
2023-08-08 13:49:08 -0300

    removing dist folder

--- backend/dist/services/user/type/user.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleType = void 0;
-var RoleType;
-(function (RoleType) {
-    RoleType["USER"] = "USER";
-    RoleType["ADMIN"] = "ADMIN";
-})(RoleType || (exports.RoleType = RoleType = {}));
-//# sourceMappingURL=user.type.js.map
